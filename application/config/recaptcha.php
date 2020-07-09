d37757c9bddb5f712ea9a81842dc71414af08358
Heri Hermawan <herhermawan007@gmail.com>
2020-07-09 22:21:15 +0700

    updaterecaptcha

--- application/config/recaptcha.php
+++ application/config/recaptcha.php
@@ -5,12 +5,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 // link: http://www.google.com/recaptcha/admin
 
 //for production
-// $config['recaptcha_site_key'] = '6LfvLWQUAAAAAGvvUiiulVM4-53ySPL8CA3Fmzlu';
-// $config['recaptcha_secret_key'] = '6LfvLWQUAAAAACR9StkDDbvecouruct7plFlWw2Y';
+$config['recaptcha_site_key'] = '6LfvLWQUAAAAAGvvUiiulVM4-53ySPL8CA3Fmzlu';
+$config['recaptcha_secret_key'] = '6LfvLWQUAAAAACR9StkDDbvecouruct7plFlWw2Y';
 
 //for localhost
-$config['recaptcha_site_key'] = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
-$config['recaptcha_secret_key'] = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe';
+// $config['recaptcha_site_key'] = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI';
+// $config['recaptcha_secret_key'] = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe';
 
 // reCAPTCHA supported 40+ languages listed here:
 // https://developers.google.com/recaptcha/docs/language
