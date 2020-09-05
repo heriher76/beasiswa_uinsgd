803d2885b0f51dd583cbbb2d0b16ee1f30ec0411
Heri Hermawan <herhermawan007@gmail.com>
2020-09-05 20:37:41 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/beasiswa_uinsgd/';
+$config['base_url'] = 'https://kipk.uinsgd.ac.id/';
 
 /*
 |--------------------------------------------------------------------------
